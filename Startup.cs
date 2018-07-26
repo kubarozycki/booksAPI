19321b0133fa715d77ca22e0c4461eb1116fc7ff
AVANADE-CORP\jakub.rozycki <jakub.rozycki@avanade.com>
2018-07-26 15:46:34 +0200

    Initial

---
+++ Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using booksAPI.Models;
+using booksAPI.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace booksAPI {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        private const string booksUrl = @"C:\\Users\\jakub.rozycki\\Repos\booksAPI\\booksAPI\\Database\\books.json";
+        private const string authorsUrl = @"C:\\Users\\jakub.rozycki\\Repos\booksAPI\\booksAPI\\Database\\authors.json";
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ();
+
+            services.AddTransient<IBaseRepository<Book>> ((provider) => {
+                return new BaseRepository<Book> (booksUrl);
+            });
+
+            services.AddTransient<IBaseRepository<Author>> ((provider) => {
+                return new BaseRepository<Author> (authorsUrl);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            }
+
+            app.UseMvc ();
+        }
+    }
+}
